7b2d8c741ffa285297f7f317b2be3f3aacf013cc
klavoie1 <kenlavoie19@gmail.com>
2026-02-09 17:10:00 -0500

    added methods for getting all songs and for deleting songs by Id

--- src/main/java/com/player/music/controller/SongController.java
+++ src/main/java/com/player/music/controller/SongController.java
@@ -1,6 +1,8 @@
 package com.player.music.controller;
 
+import com.player.music.dto.SongListResponse;
 import com.player.music.dto.SongRequest;
+import com.player.music.repository.SongRepository;
 import com.player.music.service.SongService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,8 @@ public class SongController {
 
     private final SongService songService;
 
+    private final SongRepository songRepository;
+
     @PostMapping
     public ResponseEntity<?> addSong(@RequestPart("request") String request,
                                      @RequestPart("audio") MultipartFile audioFile,
@@ -29,6 +33,29 @@ public class SongController {
             return ResponseEntity.status(HttpStatus.CREATED).body(songService.addSong(songRequest));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> listAllSongs() {
+        try {
+            return ResponseEntity.ok(songService.getAllSongs());
+        } catch (Exception e) {
+            return ResponseEntity.ok(new SongListResponse(false, null));
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> removeSong(@PathVariable String id) {
+        try {
+            boolean removed = songService.deleteSong(id);
+            if (removed) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            } else {
+                return ResponseEntity.badRequest().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
 }
