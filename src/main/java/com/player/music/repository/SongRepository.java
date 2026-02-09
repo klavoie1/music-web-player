519a605547506614b0d94d64ea246d8f9af354f2
klavoie1 <kenlavoie19@gmail.com>
2026-02-09 11:41:14 -0500

    added missing repository annotation to class

--- src/main/java/com/player/music/repository/SongRepository.java
+++ src/main/java/com/player/music/repository/SongRepository.java
@@ -2,6 +2,8 @@ package com.player.music.repository;
 
 import com.player.music.document.Song;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SongRepository extends MongoRepository<Song, String> {
 }
