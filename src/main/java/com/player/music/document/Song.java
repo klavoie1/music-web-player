3f933218d0a5c34df2af5a5d43162519df68b1ec
klavoie1 <kenlavoie19@gmail.com>
2026-02-09 09:06:42 -0500

    added song document fields

--- src/main/java/com/player/music/document/Song.java
+++ src/main/java/com/player/music/document/Song.java
@@ -1,7 +1,31 @@
 package com.player.music.document;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "songs")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Song {
+
+    @Id
+    @JsonProperty("_id")
+    private String songId;
+
+    private String songTitle;
+
+    private String songDesc;
+
+    private String songAlbum;
+
+    private String songDuration;
+
+    private String songFile;
 }
