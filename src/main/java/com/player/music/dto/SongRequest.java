a7b87b1fb09567dd5f5753c0a96939f23eee4613
klavoie1 <kenlavoie19@gmail.com>
2026-02-09 11:34:51 -0500

    created SongRequest fields

--- src/main/java/com/player/music/dto/SongRequest.java
+++ src/main/java/com/player/music/dto/SongRequest.java
@@ -1,4 +1,26 @@
 package com.player.music.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SongRequest {
+
+    private String id;
+
+    private String name;
+
+    private String description;
+
+    private String album;
+
+    private MultipartFile audioFile;
+
+    private MultipartFile coverFile;
 }
