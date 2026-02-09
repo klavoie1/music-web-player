2491c44c8b4cff2262e84bc251fbbdac422a333f
klavoie1 <kenlavoie19@gmail.com>
2026-02-09 16:45:21 -0500

    added addSong POST method

--- src/main/java/com/player/music/controller/SongController.java
+++ src/main/java/com/player/music/controller/SongController.java
@@ -1,11 +1,35 @@
 package com.player.music.controller;
 
+import com.player.music.document.Song;
+import com.player.music.dto.SongRequest;
+import com.player.music.service.SongService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import tools.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/api/songs")
 @RequiredArgsConstructor
 public class SongController {
+
+    private final SongService songService;
+
+    @PostMapping
+    public ResponseEntity<?> addSong(@RequestPart("request") String request,
+                                     @RequestPart("audio") MultipartFile audioFile,
+                                     @RequestPart("image") MultipartFile imageFile) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            SongRequest songRequest = objectMapper.readValue(request, SongRequest.class);
+            songRequest.setCoverFile(imageFile);
+            songRequest.setAudioFile(audioFile);
+            songService.addSong(songRequest);
+            return ResponseEntity.status(HttpStatus.CREATED).body(songService.addSong(songRequest));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
