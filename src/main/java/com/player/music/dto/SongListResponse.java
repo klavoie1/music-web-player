840672038a44228d14e19f9b09b4d0b33d99591d
klavoie1 <kenlavoie19@gmail.com>
2026-02-09 11:35:04 -0500

    created SongListResponse fields

--- src/main/java/com/player/music/dto/SongListResponse.java
+++ src/main/java/com/player/music/dto/SongListResponse.java
@@ -1,4 +1,20 @@
 package com.player.music.dto;
 
+import com.player.music.document.Song;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SongListResponse {
+
+    private Boolean success;
+
+    private List<Song> songList;
 }
