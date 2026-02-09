99490861e594d0964d0ccf66072bcf9f443b774b
klavoie1 <kenlavoie19@gmail.com>
2026-02-09 11:40:58 -0500

    initial commit

--- src/main/java/com/player/music/controller/SongController.java
+++ src/main/java/com/player/music/controller/SongController.java
@@ -1,4 +1,11 @@
 package com.player.music.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/songs")
+@RequiredArgsConstructor
 public class SongController {
 }
