71edc17f03e32877e089cd4241f29d0d8b3e42f1
klavoie1 <kenlavoie19@gmail.com>
2026-02-09 21:22:29 -0500

    cors rules added

--- src/main/java/com/player/music/config/SecurityConfig.java
+++ src/main/java/com/player/music/config/SecurityConfig.java
@@ -6,12 +6,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
     /**
-    For any URI within the website, Spring Boot Blocks any attempt to connect to said URI without
+     For any URI within the website, Spring Boot Blocks any attempt to connect to said URI without
      correct authorization. This code ensures that only certain URI's that are passed here are allowed
      by general users. Other requests will need to be ones that are accessed though authorized accounts.
      * */
@@ -24,5 +29,23 @@ public class SecurityConfig {
                 ).permitAll().anyRequest().authenticated());
 
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter(urlBasedCorsConfigurationSource());
+    }
+
+
+    private UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*");
+        corsConfig.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", corsConfig);
+        return configSource;
     }
 }
